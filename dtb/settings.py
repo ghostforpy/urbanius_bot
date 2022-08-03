40bbd28bdece75885ff65610e13cd3a94e0619df
Vitaly Obrubov <obrubov@yandex.ru>
2022-08-03 13:54:41 +0300

    3333

--- dtb/settings.py
+++ dtb/settings.py
@@ -85,7 +85,14 @@ ASGI_APPLICATION = 'dtb.asgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PWD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
+    }
 }
 
 
