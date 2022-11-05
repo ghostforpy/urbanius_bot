ed4b978828a4867e1f7188a561df055d9f3c1ee5
ghost <ghostformobile@gmail.com>
2022-11-05 13:02:17 +0300

    fix

--- dtb/urls.py
+++ dtb/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from dtb import startup_code
-from tgbot import set_webhook_url
+#from tgbot import set_webhook_url
 
 
 urlpatterns = [
